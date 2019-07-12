28358d5676f81688684353ee7a5d08cec09fb9f2
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-07-12 16:18:53 -0600

    Create wf_vasp_bulk_mag_relax.py

---
+++ wf_vasp_bulk_mag_relax.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 13:27:40 2019
+Creates magnetic structures and vasp input including bulk relax convergence file.
+@author: Zach Bare
+"""
+
+
+import os
+from pymatgen.io.vasp.sets import MPRelaxSet,batch_write_input
+from pymatgen.io.vasp.inputs import Poscar
+from materials-workflows.magnetism.analyzer import MagneticStructureEnumerator
+from shutil import copyfile
+
+def gen_input():
+  pwd = os.getcwd()
+  structure = Poscar.from_file(os.path.join(pwd,'POSCAR'))
+  mag_structures = MagneticStructureEnumerator(structure)
+  batch_write_input(mag_structures.ordered_structures, vasp_input_set=MPRelaxSet,
+                    output_dir=os.path.join(pwd,'mag_bulk_vasp_relax'))
+
+for root, dirs, files in os.walk(pwd):
+    for file in files:
+        if file == 'POTCAR':
+            copyfile('CONVERGENCE',os.path.join(pwd,'CONVERGENCE'))
