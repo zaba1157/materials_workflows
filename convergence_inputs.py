d805586409b3c941cfa81aaa5ff3ce0c5609668b
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-07-12 22:18:35 -0600

    Update convergence_inputs.py

--- convergence_inputs.py
+++ convergence_inputs.py
@@ -6,14 +6,14 @@ Created on Fri Jul 12 13:27:40 2019
 @author: Zach Bare
 """
 
-def surf_convergence(final_kpoints):
+def surf_convergence(inital_kpoints, final_kpoints):
 
   step0 = ['\n0 Very_Rough_Converge\n',
            'PREC = Normal','ENCUT = 520','NSW = 5000',
            'EDIFFG = -0.8','EDIFF = 1e-3','NELMIN = 10',
            'NELM = 80','ALGO = Fast','LORBIT = 11','IOPT = 7',
            'IBRION = 3','POTIM = 0','ISTART = 0','ICHARG = 2',
-           'ISMEAR = 0','ISYM = 0','\nKPOINTS 1 1 1']
+           'ISMEAR = 0','ISYM = 0','\nKPOINTS '+str(inital_kpoints)]
   step1 = ['\n1 Rough_Converge\n',
            'EDIFF = 1e-5','EDIFFG = -0.1','NELMIN = 4',
            'NELM = 60','ISMEAR = -5','\nKPOINTS '+str(final_kpoints)]
@@ -22,6 +22,12 @@ def surf_convergence(final_kpoints):
            'PREC = Accurate','NELM = 100']
   step3 = ['\n3 One_Step\n','LAECHG = .TRUE.','NSW = 0','NELM = 500']
   
+  return step0 + step1 + step2 + step3
+
+
+def bulk_convergence(inital_kpoints, final_kpoints):
+  
+  step3 = ['\n3 One_Step\n','LAECHG = .TRUE.','NSW = 0','NELM = 500']
   return step0 + step1 + step2 + step3
 
 
