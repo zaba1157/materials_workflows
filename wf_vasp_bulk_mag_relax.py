dd425e0297450c462c9ca300423f55994ee5ac0c
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-07-12 21:36:44 -0600

    Update wf_vasp_bulk_mag_relax.py

--- wf_vasp_bulk_mag_relax.py
+++ wf_vasp_bulk_mag_relax.py
@@ -8,12 +8,14 @@ Creates magnetic structures and vasp input including bulk relax convergence file
 
 
 import os
+import argparse
 from pymatgen.io.vasp.sets import MPRelaxSet,batch_write_input
 from pymatgen.io.vasp.inputs import Poscar
-from materials-workflows.magnetism.analyzer import MagneticStructureEnumerator
-from shutil import copyfile, move
-from materials-workflows.vasp_functions import *
-from materials-workflows.convergence_inputs import bulk_convergence
+from materials_workflows.magnetism.analyzer import MagneticStructureEnumerator
+from shutil import  move
+from materials_workflows.vasp_functions import *
+from materials_workflows.convergence_inputs import bulk_convergence
+
 
 def gen_input():
   pwd = os.getcwd()
@@ -23,17 +25,11 @@ def gen_input():
                     output_dir=os.path.join(pwd,'vasp_bulk_mag_relax'))
   for root, dirs, files in os.walk(os.path.join(pwd,'vasp_bulk_mag_relax')):
       for file in files:
-          if file == 'KPOINTS':
-              with open('KPOINTS','r') as k:
-                  linecount = 0
-                  for line in k:
-                      if linecount == 3:
-                          kpts_line = line
-                          break
-                      linecount+=1
-              k.close()
-              convergence_writelines = bulk_convergence(str(kpts_line))
-              write_vasp_convergence_file(root,convergence_writelines)
+        if file == POSCAR:
+          kpoints1 = get_kpoints(os.path.join(root,'POSCAR'), 500)
+          kpoints2 = get_kpoints(os.path.join(root,'POSCAR'), 1000)
+          convergence_writelines = bulk_convergence(kpoints1,kpoints2)
+          write_vasp_convergence_file(root,convergence_writelines)
   
 def check_converged():
   pwd = os.getcwd()
@@ -47,4 +43,30 @@ def check_converged():
           move(os.path.join(root,file),job_to_pass)  
   else:
     write_workflow_convergence_file(pwd, False)
+    
+    
+def rerun_task():
+  #only needed for non-VASP calculations
+  pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--gen_inputs', help='Generates inputs for workflow.',
+                        action='store_true')
+    parser.add_argument('-c', '--converged', help='Checks for convergence of workflow.',
+                        action='store_true')
+    parser.add_argument('-r', '--rerun', help='Reruns worklow. This does nothing if vasp workflow.',
+                        action='store_true')
+    args = parser.parse_args()
+    
+    if args.gen_inputs:
+      gen_input()
+    elif args.converged:
+      check_converged()
+    else:
+      rerun_task()
+
+
+
   
