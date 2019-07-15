6413d5ca58a7d3b8ab3c9b8cb551fb22c3c1918d
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-07-14 18:10:35 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 18:05:24 2019
+
+@author: zach1
+"""
+
+import os
+
+scripts = ['vasp_bulk_mag_relax.py','workflow_rerun.py']
+
+for s in scripts:
+    os.system('chmod 755 '+s)
