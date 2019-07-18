ff43f2e3f60de9ce6ecb07be2501d57d02dfd5bc
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-07-18 15:35:54 -0600

    Delete vasp_band_structure.WORKFLOW_COMMANDS.py

--- materials_workflows/workflows/vasp_band_structure.WORKFLOW_COMMANDS.py
+++
@@ -1,6 +0,0 @@
-Commands should be entered comma seperated with no extra spaces. File will not be parsed correctly if this is not done!
-
-Step,   Generate Inputs Command,        Check Convergence Command,      Rerun Command,  Workflow Name
-
-0,vasp_bulk_mag_relax.py -i,vasp_bulk_mag_relax.py -c,vasp_run,bulk_mag
-1,vasp_band_structure.py -i,vasp_band_structure.py -c,vasp_run,band_structure
