41355775cc3f68c312713b407820baa148b2a639
rymo1354 <52838869+rymo1354@users.noreply.github.com>
2019-07-19 08:38:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Created on Sun Jul 14 18:05:24 2019
 
 @author: zach1
 """
-
+# makes the scripts listed executables
 import os
 
 scripts = ['vasp.py','vasp_bulk_mag_relax.py', 'vasp_band_structure.py', 'workflow_rerun.py']
