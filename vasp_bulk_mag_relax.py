3c4630e9cbfc3e4f21fdbd4177799868c8d7232f
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-07-26 12:54:27 -0600

    Update vasp_bulk_mag_relax.py

--- vasp_bulk_mag_relax.py
+++ vasp_bulk_mag_relax.py
@@ -33,7 +33,8 @@ def gen_input():
         if file == 'POSCAR':
           kpoints1 = get_kpoints(os.path.join(root,'POSCAR'), 300)
           kpoints2 = get_kpoints(os.path.join(root,'POSCAR'), 1000)
-          convergence_writelines = bulk_convergence(kpoints1,kpoints2)
+          natoms = len(Poscar.from_file(os.path.join(root,'POSCAR')).structure)
+          convergence_writelines = bulk_convergence(kpoints1,kpoints2,natoms)
           write_vasp_convergence_file(root,convergence_writelines)
   
 def check_converged():
