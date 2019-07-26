d76a1b773c29a8588718c899be072527b01afaaa
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-07-26 13:25:53 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Sun Jul 14 18:05:24 2019
 # makes the scripts listed executables
 import os
 
-scripts = ['vasp.py','vasp_bulk_mag_relax.py', 'vasp_band_structure.py', 'workflow_rerun.py']
+scripts = ['vasp.py','vasp_bulk_mag_relax.py', 'vasp_band_structure.py', 'workflow_rerun.py','vasp_bulk_volume_relax.py']
 
 for s in scripts:
     os.system('chmod 755 '+s)
