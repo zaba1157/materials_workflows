c09d6949a7ecd52ff319dc595ac9b551af61f8b8
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-08-02 17:16:34 -0600

    Update vasp_bulk_mag_relax.py

--- vasp_bulk_mag_relax.py
+++ vasp_bulk_mag_relax.py
@@ -12,28 +12,34 @@ import argparse
 from pymatgen.io.vasp.sets import MPRelaxSet,batch_write_input
 from pymatgen.io.vasp.inputs import Poscar
 from materials_workflows.magnetism.analyzer import MagneticStructureEnumerator
-from shutil import  move
-from materials_workflows.vasp_functions import *
+from materials_workflows.vasp_functions import get_previous_pass_path, get_structure_from_pass_path, write_workflow_convergence_file
+from materials_workflows.vasp_functions import get_kpoints, write_vasp_convergence_file, workflow_is_converged
+from materials_workflows.vasp_functions import get_minimum_energy_job, move_job_to_pass_path
 from materials_workflows.vasp_convergence.convergence_inputs import bulk_convergence
 
+################################################
+
+# Define Global Variables #
+
+workflow_name = 'bulk_mag'
+
+pwd = os.getcwd()
+workflow_path = os.path.join(pwd,workflow_name)
+
+################################################
 
 def gen_input():
-  pwd = os.getcwd()
-  workflow_name = 'bulk_mag'
-  stage_number = get_workflow_stage_number_from_name(wf_command_path, workflow_name)
-  #if stage_number == 0:
-    
-  workflow_path = os.path.join(pwd,workflow_name)
+  
   os.mkdir(workflow_path)
-  structure = Poscar.from_file(os.path.join(pwd,'POSCAR')).structure
-  move(os.path.join(pwd,'POSCAR'),os.path.join(pwd,'POSCAR.orig'))
+  start_path = get_previous_pass_path(pwd,workflow_name)
+  structure = get_structure_from_pass_path(start_path) 
   write_workflow_convergence_file(workflow_path, False)
   mag_structures = MagneticStructureEnumerator(structure)
   batch_write_input(mag_structures.ordered_structures, vasp_input_set=MPRelaxSet,
                     output_dir=workflow_path)
   for root, dirs, files in os.walk(workflow_path):
       for file in files:
-        if file == 'POSCAR':
+        if file == 'POTCAR':
           kpoints1 = get_kpoints(os.path.join(root,'POSCAR'), 300)
           kpoints2 = get_kpoints(os.path.join(root,'POSCAR'), 1000)
           natoms = len(Poscar.from_file(os.path.join(root,'POSCAR')).structure)
@@ -41,18 +47,12 @@ def gen_input():
           write_vasp_convergence_file(root,convergence_writelines)
   
 def check_converged():
-  pwd = os.getcwd()
-  workflow_name = 'bulk_mag'
-  workflow_path = os.path.join(pwd,workflow_name)
-  #os.chdir(workflow_path)
+  
   if workflow_is_converged(workflow_path) == True:
     write_workflow_convergence_file(workflow_path, True)
-    job_path = get_minimum_energy_job(workflow_path)
-    stage_number = get_workflow_stage_number(pwd)
-    job_to_pass = os.path.join(pwd,str(stage_number)+'_'+str(workflow_name)+'_final')
-    for root, dirs, files in os.walk(job_path):
-        for file in files:
-          move(os.path.join(root,file),job_to_pass)  
+    minE_job_path = get_minimum_energy_job(workflow_path)
+    move_job_to_pass_path(pwd,minE_job_path,workflow_name)
+ 
   else:
     write_workflow_convergence_file(workflow_path, False)
     
@@ -78,7 +78,4 @@ if __name__ == '__main__':
       check_converged()
     else:
       rerun_task()
-
-
-
-  
+      
