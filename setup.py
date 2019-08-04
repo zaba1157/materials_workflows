0bf4ae483012494bcea26aa1baf6e4c1401ac8ba
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-08-04 14:41:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 """
 Created on Sun Jul 14 18:05:24 2019
 
-@author: zach1
+@author: Zach Bare
 """
-# makes the scripts listed executables
+# makes the scripts listed executable
+
 import os
 
-scripts = ['vasp.py','vasp_bulk_mag_relax.py', 'vasp_band_structure.py',
+
+scripts = ['vasp_bulk_mag_relax.py', 'vasp_band_structure.py',
            'workflow_rerun.py','vasp_bulk_volume_relax.py','FM_bulk_relax.py']
 
+if str(os.environ["VASP_COMPUTER"]) == 'summit':
+    scripts.append('vasp.py')
+    
 for s in scripts:
     os.system('chmod 755 '+s)
