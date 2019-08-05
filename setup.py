e1aa7b1b09e6d91ec482f7b013dacafed77fd630
rymo1354 <52838869+rymo1354@users.noreply.github.com>
2019-08-05 14:14:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,14 @@ Created on Sun Jul 14 18:05:24 2019
 
 @author: Zach Bare
 """
-# makes the scripts listed executable
 
 import os
 from pathlib import Path
 
 scripts = [script for script in os.listdir('.') if os.path.isfile(script) and Path(script).suffix == '.py']
 
-'''
-if str(os.environ["VASP_COMPUTER"]) == 'summit':
-    scripts.append('vasp.py')
-'''    
 for s in scripts:
     os.system('chmod 755 '+s)
+
+# makes all python scripts (including setup.py) in the present working directory executable
+# run with command line command python3 setup.py, if scripts turn green in terminal it was performed correctly
