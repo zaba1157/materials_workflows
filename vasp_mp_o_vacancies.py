5c5f1cc11a31838868a927a004e70ea75a9f1c07
rymo1354 <52838869+rymo1354@users.noreply.github.com>
2019-08-08 13:12:17 -0600

    Update vasp_mp_o_vacancies.py

--- vasp_mp_o_vacancies.py
+++ vasp_mp_o_vacancies.py
@@ -4,9 +4,10 @@
 
 import os
 import argparse
+from pymatgen.io.vasp.inputs import Poscar
 from materials_workflows.vasp_functions import write_workflow_convergence_file, workflow_is_converged
 from materials_workflows.vasp_functions import get_mpids_from_file, get_structures_from_materials_project
-from materials_workflows.vasp_functions import structure_scaler, append_to_incars_and_write_convergence_files
+from materials_workflows.vasp_functions import structure_scaler, append_to_incars
 from materials_workflows.vasp_functions import get_structures_with_element_removed
 from pymatgen.io.vasp.sets import MPRelaxSet, batch_write_input
 
@@ -40,7 +41,17 @@ def generate_input_files(filename, mp_key, to_scale=True):
         batch_write_input(structure_list, vasp_input_set=MPRelaxSet, output_dir=compound_path,
                           make_dir_if_not_present=True)
                           
-    append_to_incars_and_write_convergence_files(pwd, tags_to_add, os.path.basename(__file__))
+    append_to_incars(pwd, tags_to_add)
+    
+    for root, dirs, files in os.walk(workflow_path):
+        for file in files:
+            if file == 'POTCAR':
+                kpoints1 = get_kpoints(os.path.join(root, 'POSCAR'), 300)
+                kpoints2 = get_kpoints(os.path.join(root, 'POSCAR'), 1000)
+                natoms = len(Poscar.from_file(os.path.join(root, 'POSCAR')).structure)
+                convergence_writelines = bulk_convergence(kpoints1, kpoints2, natoms)
+                write_vasp_convergence_file(root, convergence_writelines)
+                
     write_workflow_convergence_file(workflow_path, False)
 
     return
