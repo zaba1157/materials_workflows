4a3d944aa95634ff539fe60d8a9c36f952b7327e
rymo1354 <52838869+rymo1354@users.noreply.github.com>
2019-08-22 09:27:16 -0600

    Update vasp_bulk_volume_relax.py

--- vasp_bulk_volume_relax.py
+++ vasp_bulk_volume_relax.py
@@ -9,14 +9,15 @@ Created on Wed Jul 24 12:02:08 2019
 
 import os
 import argparse
-from pymatgen.io.vasp.sets import MPRelaxSet,batch_write_input
+from pymatgen.io.vasp.sets import MPRelaxSet, batch_write_input
 from pymatgen.io.vasp.inputs import Poscar
 from materials_workflows.magnetism.analyzer import MagneticStructureEnumerator
 from shutil import  move, copy
 from materials_workflows.magnetism.analyzer import MagneticStructureEnumerator
 from materials_workflows.vasp_functions import get_previous_pass_path, get_structure_from_pass_path, write_workflow_convergence_file
 from materials_workflows.vasp_functions import get_kpoints, write_vasp_convergence_file, workflow_is_converged
-from materials_workflows.vasp_functions import get_minimum_energy_job, move_job_to_pass_path
+from materials_workflows.vasp_functions import get_minimum_energy_job, move_job_to_pass_path, append_to_incars
+from materials_workflows.vasp_functions import get_paths_from_file, get_structures_from_paths
 from materials_workflows.vasp_convergence.convergence_inputs import volume_bulk_convergence
 
 ################################################
@@ -24,53 +25,88 @@ from materials_workflows.vasp_convergence.convergence_inputs import volume_bulk_
 ''' Define Global Variables '''
 
 workflow_name = 'vol_rlx'
-
+initial_scale_factors = [0.8, 1.0, 1.2, 1.4, 1.6] 
+paths_filename = 'PATHS'
 pwd = os.getcwd()
-workflow_path = os.path.join(pwd,workflow_name)
-start_path = get_previous_pass_path(pwd,workflow_name)
+workflow_path = os.path.join(pwd, workflow_name)
+start_path = get_previous_pass_path(pwd, workflow_name)
 
 ################################################
 
 
-def gen_input():
+def gen_input(scale_factors):
 
-  os.mkdir(workflow_path)
-  structure = get_structure_from_pass_path(start_path) 
-  write_workflow_convergence_file(workflow_path, False
-  inital_scale_factors = [0.8,1.0,1.2,1.4,1.6] 
-  init_volume = structure.volume
-  scaled_structures = []
-  for sf in inital_scale_factors:
-      structure.scale_lattice(sf*init_volume)
-      scaled_structures.append(structure)
-  
-  batch_write_input(scaled_structures, vasp_input_set=MPRelaxSet,
-                    output_dir=workflow_path)
-  for root, dirs, files in os.walk(workflow_path):
-      for file in files:
-        if file == 'POTCAR':
-          kpoints1 = get_kpoints(os.path.join(root,'POSCAR'), 300)
-          kpoints2 = get_kpoints(os.path.join(root,'POSCAR'), 1000)
-          convergence_writelines = volume_bulk_convergence(kpoints1,kpoints2)
-          write_vasp_convergence_file(root,convergence_writelines)                                  
-                                  
-def check_converged():
-  min_num_converged_vols = 4
-  max_num_submission_failures = 2
-  vol_tolerance = 1 #ang^3
-  
-  if volume_workflow_is_converged(pwd,max_num_submission_failures,min_num_converged_vols,vol_tolerance) == True:
-    write_workflow_convergence_file(workflow_path, True)
-    minE_job_path = get_minimum_energy_job(workflow_path)
-    move_job_to_pass_path(pwd,minE_job_path,workflow_name)
-  else:
+    os.mkdir(workflow_path)
+    structure = get_structure_from_pass_path(start_path) 
     write_workflow_convergence_file(workflow_path, False)
+    
+    init_volume = structure.volume
+    scaled_structures = []
+    for sf in scale_factors:
+        structure.scale_lattice(sf*init_volume)
+        scaled_structures.append(structure)
+  
+    batch_write_input(scaled_structures, vasp_input_set=MPRelaxSet,
+                      output_dir=workflow_path)
+  
+    for root, dirs, files in os.walk(workflow_path):
+        for file in files:
+            if file == 'POTCAR':
+                kpoints1 = get_kpoints(os.path.join(root, 'POSCAR'), 300)
+                kpoints2 = get_kpoints(os.path.join(root, 'POSCAR'), 1000)
+                convergence_writelines = volume_bulk_convergence(kpoints1, kpoints2)
+                write_vasp_convergence_file(root, convergence_writelines)
+    
+    return
+
+def read_input_files(filename, scale_factors):
+
+    if os.path.isdir(workflow_path) == False:
+        os.mkdir(workflow_path)
+
+    paths_list = get_paths_from_file(os.path.join(pwd, filename))
+    structures = get_structures_from_paths(paths_list) #list of structures from filename
+    
+    for structure in structures:
+        scaled_structures = []
+        init_volume = structure.volume
+        for sf in scale_factors:
+            structure.scale_lattice(sf*init_volume)
+            scaled_structures.append(structure)
+
+        batch_write_input(scaled_structures, vasp_input_set=MPRelaxSet, output_dir=workflow_path,
+                          make_dir_if_not_present=True)
+    append_to_incars(pwd, tags_to_add)
+
+    for root, dirs, files in os.walk(workflow_path):
+        for file in files:
+            if file == 'POTCAR':
+                kpoints1 = get_kpoints(os.path.join(root, 'POSCAR'), 300)
+                kpoints2 = get_kpoints(os.path.join(root, 'POSCAR'), 1000)
+                natoms = len(Poscar.from_file(os.path.join(root, 'POSCAR')).structure)
+                convergence_writelines = bulk_convergence(kpoints1, kpoints2, natoms)
+                write_vasp_convergence_file(root, convergence_writelines)
+
+    write_workflow_convergence_file(workflow_path, False)
+
+    return
+  
+def check_converged():
+    min_num_converged_vols = 4
+    max_num_submission_failures = 2
+    vol_tolerance = 1 #ang^3
+  
+    if volume_workflow_is_converged(pwd, max_num_submission_failures, min_num_converged_vols, vol_tolerance) == True:
+        write_workflow_convergence_file(workflow_path, True)
+        minE_job_path = get_minimum_energy_job(workflow_path)
+        move_job_to_pass_path(pwd, minE_job_path, workflow_name)
+    else:
+        write_workflow_convergence_file(workflow_path, False)
     
     
 def rerun_task():
-  #only needed for non-VASP calculations
-  pass
-
+    #only needed for non-VASP calculations
+    pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -78,14 +114,18 @@ if __name__ == '__main__':
                         action='store_true')
     parser.add_argument('-c', '--converged', help='Checks for convergence of workflow.',
                         action='store_true')
+    parser.add_argument('-d', '--read_inputs', help='Reads inputs for workflow.',
+                        action='store_true')
     parser.add_argument('-r', '--rerun', help='Reruns worklow. This does nothing if vasp workflow.',
                         action='store_true')
     args = parser.parse_args()
     
     if args.gen_inputs:
-      gen_input()
+        gen_input(initial_scale_factors)
+    elif args.read_inputs:
+        read_input_files(paths_filename, initial_scale_factors)
     elif args.converged:
-      check_converged()
+        check_converged()
     else:
-      rerun_task()
+        rerun_task()
 
