73747ab2440ed89f098f153f943437af862f4a4c
rymo1354 <52838869+rymo1354@users.noreply.github.com>
2019-08-22 23:22:40 -0600

    Update substituted_structures_from_SQS.py

--- scripts/substituted_structures_from_SQS.py
+++ scripts/substituted_structures_from_SQS.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Aug 22 20:25:03 2019
-
 @author: rymo1354
 """
 from pymatgen.io.vasp.inputs import Poscar
@@ -17,7 +16,7 @@ elements_to_include = ['Li', 'Be', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ti', 'V',
                        'Ta', 'W', 'Bi', 'La', 'Ce', 'Pr', 'Nd', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'O']
 
 def get_Structure_Template(template_path):
-    
+
     return Poscar.from_file(template_path).structure
 
 def get_Dummies_and_Elements(struct):
@@ -35,11 +34,11 @@ def get_Dummies_and_Elements(struct):
     return dummies, species
 
 def get_Species_Substitution_Order(structure, dummies, species, to_ignore):
-    
+
     els_removed = species.copy()
     for element in to_ignore:
         els_removed.remove(Element(element))
-    
+
     subs = [list(p) for p in permutations(els_removed)]
     diff_coords = []
     struct_copies = []
@@ -53,8 +52,8 @@ def get_Species_Substitution_Order(structure, dummies, species, to_ignore):
         for element_ind in range(len(copy.species)):
             cn = CN.get_cn(copy, element_ind, use_weights=True)
             coordination_numbers.append((copy.species[element_ind], cn))
-        diff_coords.append(coordination_numbers)   
-    
+        diff_coords.append(coordination_numbers)
+
     unique_cn_els = []
     for cn in diff_coords:
         cns = []
@@ -62,7 +61,7 @@ def get_Species_Substitution_Order(structure, dummies, species, to_ignore):
             if cn[i] not in cns:
                 cns.append(cn[i])
         unique_cn_els.append(cns)
-        
+
     substitution_ind = None
     check_len = 300 # arbitrarily high number to start, used to get substitution scheme with lowest number of unique sites
     for unique_cn_el_ind in range(len(unique_cn_els)):
@@ -71,21 +70,21 @@ def get_Species_Substitution_Order(structure, dummies, species, to_ignore):
             check_len = len(unique_cn_els[unique_cn_el_ind])
         else:
             continue
-    
+
     ox_transform = AutoOxiStateDecorationTransformation()
     ox_species = ox_transform.apply_transformation(struct_copies[substitution_ind]).types_of_specie
-    for specie in ox_species: 
+    for specie in ox_species:
         for element in to_ignore:
             if specie.symbol == Element(element).symbol:
                 ox_species.remove(specie)
-    
+
     return subs[substitution_ind], ox_species
 
 def get_Probable_Substitutions(species, elements_to_include, threshold):
-    
+
     Sub = Substitutor(threshold=threshold)
     subs_dict = {}
-    
+
     for specie in species:
         subs = Sub.pred_from_list([specie])
         try:
@@ -98,32 +97,39 @@ def get_Probable_Substitutions(species, elements_to_include, threshold):
                     subs_dict[subs_dict_key].append(list(subs[i]['substitutions'].values())[0])
         except:
             print('No probable substitutions at %s threshold for %s' % (threshold, specie))
-        
+            continue
+
     return subs_dict
 
 def probable_Substitution_Structures(template_structure, dummies, ordered_subs, probable_substitutions):
-    
+
     combos = list(product(*list(probable_substitutions.values())))
-    
+
     probable_structures = []
     for combo in combos:
         copy = template_structure.copy()
         for dummy_ind in range(len(dummies)):
             copy[dummies[dummy_ind].symbol] = combo[dummy_ind].symbol
-        
+
         if len(copy.composition.oxi_state_guesses()) > 0:
             probable_structures.append(copy)
         else:
             continue
-    
+
     return probable_structures
 
 def get_Structures(template_path, elements_to_ignore, elements_to_include=elements_to_include, threshold=0.038):
-    
+
     structure = get_Structure_Template(template_path)
     dummies, species = get_Dummies_and_Elements(structure)
-    ordered_subs, ox_species = get_Species_Substitution_Order(structure, dummies, species, elements_to_ignore)
-    probable_substitutions = get_Probable_Substitutions(ox_species, elements_to_include, threshold)
-    probable_structures = probable_Substitution_Structures(structure, dummies, ordered_subs, probable_substitutions)
-    
-    return probable_structures
+    print(dummies, species)
+    try:
+        ordered_subs, ox_species = get_Species_Substitution_Order(structure, dummies, species, elements_to_ignore)
+        print(ordered_subs, ox_species)
+        probable_substitutions = get_Probable_Substitutions(ox_species, elements_to_include, threshold)
+        probable_structures = probable_Substitution_Structures(structure, dummies, ordered_subs, probable_substitutions)
+        return probable_structures
+
+    except:
+        print('No probable substitutions at %s threshold for %s' % (threshold, structure.composition))
+        return None
