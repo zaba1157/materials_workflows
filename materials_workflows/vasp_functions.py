4680a9adece36dc2c0f7f88549ef7ed2791af92b
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2019-10-09 13:25:13 -0600

    Update vasp_functions.py

--- materials_workflows/vasp_functions.py
+++ materials_workflows/vasp_functions.py
@@ -411,8 +411,8 @@ def is_converged(path):
         else:
             raise Exception('Copy CONVERGENCE file into execution directory to run multistep job. Delete STAGE_NUMBER tag from INCAR for single step job.')
 
-        current_stage_number = get_incar_value(path, 'STAGE_NUMBER')
-        ediffg = get_incar_value(path,'EDIFFG')
+        current_stage_number = int(get_incar_value(path, 'STAGE_NUMBER'))
+        ediffg = float(get_incar_value(path,'EDIFFG'))
         if current_stage_number < max_stage_number:
             rerun = 'multi'    #RERUN JOB
         elif current_stage_number == max_stage_number:
