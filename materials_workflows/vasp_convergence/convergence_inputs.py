4fcd56c1e8e96a5cf19e1fb44816e58f5bd27d1c
zaba1157 <51424396+zaba1157@users.noreply.github.com>
2020-01-13 18:24:32 -0700

    Update convergence_inputs.py

--- materials_workflows/vasp_convergence/convergence_inputs.py
+++ materials_workflows/vasp_convergence/convergence_inputs.py
@@ -20,7 +20,7 @@ def Sam_scan_convergence(inital_kpoints, final_kpoints, natoms, tags_to_remove, 
     
     #add_tags_string = list(','.join([str(t) for t in tags_to_add])) #string of INCAR tags to add #may not work
     
-     if natoms < 12:
+    if natoms < 12:
         npar = 'NPAR = 3'
         kpar = 'KPAR = 4'
         auto_nodes = 'AUTO_NODES = 1'
@@ -34,26 +34,53 @@ def Sam_scan_convergence(inital_kpoints, final_kpoints, natoms, tags_to_remove, 
         auto_nodes = 'AUTO_NODES = 1' 
         
 
-    step0 = ['\n0 Init_Converge\n', 'LDAU = False', 'METAGGA = SCAN',
+    step0 = ['\n0 Init_Converge\n',npar,kpar,auto_nodes,
+             'LDAU = False', 'METAGGA = SCAN',
              'LASPH = True', 'ADDGRID = True',
              # add strings...
              # other settings to change....
              'NSW = 500','ISMEAR = 0','SIGMA=0.05',
              'EDIFFG = -3E-2','EDIFF = 1e-6','ALGO = Fast',
              'LORBIT = 11','IOPT = 7','IBRION = 3','POTIM = 0',
-             'ISMEAR = 0','ISYM = 0',
+             'ISYM = 0',
              # can do other kpoints in seperate steps
              '\nKPOINTS '+str(final_kpoints),
              #tags to be removed
             '\nREMOVE '+ remove_tags_string] # the \n are spaces just so it looks better 
     
     step1 = ['\n1 Next_Converge\n',#other settings...
-             '\nKPOINTS '+str(final_kpoints)] 
+             '\nKPOINTS '+str(final_kpoints)] # not implemented here
     
-    return step0 
-    
-    
-    
+    return step0
+
+
+
+
+
+def MP_bulk_convergence(natoms):
+
+    if natoms < 12:
+        npar = 'NPAR = 3'
+        kpar = 'KPAR = 4'
+        auto_nodes = 'AUTO_NODES = 1'
+    elif natoms > 100:
+        npar = ' '
+        kpar = ' '
+        auto_nodes = 'AUTO_NODES = 2'
+    else:
+        npar = ' '
+        kpar = ' '
+        auto_nodes = 'AUTO_NODES = 1'
+
+    step0 = ['\n0 Init\n', npar,kpar,auto_nodes]
+
+    step1 = ['\n1 Run2\n']
+
+    return step0 + step1
+
+
+
+
 
 def surf_convergence(inital_kpoints, final_kpoints):
 
@@ -75,6 +102,77 @@ def surf_convergence(inital_kpoints, final_kpoints):
 
 
 def bulk_convergence(inital_kpoints, final_kpoints, natoms):
+    
+    if natoms < 12:
+        npar = 'NPAR = 3'
+        kpar = 'KPAR = 4'
+        auto_nodes = 'AUTO_NODES = 1'
+    elif natoms > 100:
+        npar = ' '
+        kpar = ' '
+        auto_nodes = 'AUTO_NODES = 2'
+    else:
+        npar = ' '
+        kpar = ' '
+        auto_nodes = 'AUTO_NODES = 1'  
+        
+    step0 = ['\n0 Very_Rough_Converge\n', npar,kpar,auto_nodes,
+             'EDIFFG = -0.1', 'NSW = 500','ISTART = 0', 'LORBIT = 11',
+             'ISYM = 2', 'ALGO = Fast', 'PREC = Normal', 'ISMEAR = 0',
+             'SIGMA = 0.05', 'IOPT = 7', 'POTIM = 0', 'IBRION = 3',
+             'ISIF = 3','\nKPOINTS '+str(inital_kpoints)]
+    
+    step1 = ['\n1 Rough_Converge\n',
+             'EDIFFG = -0.05', 'ISYM = 0','NELMIN = 4','NELM = 60',
+             'PREC = Accurate',
+             '\nKPOINTS '+str(final_kpoints)]
+      
+    step2 = ['\n2 One_Step\n',
+             'LAECHG = .TRUE.','NSW = 0','NELM = 500','ICHARG = 0','ISTART = 1',
+             'LWAVE = False', 'LVHAR = True', 'ALGO = Normal','NELMIN = 10']
+    
+    return step0 + step1 + step2
+
+def old_1bulk_convergence(inital_kpoints, final_kpoints, natoms):
+    
+    if natoms < 12:
+        npar = 'NPAR = 3'
+        kpar = 'KPAR = 4'
+        auto_nodes = 'AUTO_NODES = 1'
+    elif natoms > 100:
+        npar = ' '
+        kpar = ' '
+        auto_nodes = 'AUTO_NODES = 2'
+    else:
+        npar = ' '
+        kpar = ' '
+        auto_nodes = 'AUTO_NODES = 1'  
+        
+    step0 = ['\n0 Very_Rough_Converge\n', npar,kpar,auto_nodes,
+             'EDIFFG = -0.1', 'NSW = 500','ISTART = 0', 'LORBIT = 11',
+             'ISYM = 2', 'ALGO = Fast', 'PREC = Normal', 'ISMEAR = 0',
+             'SIGMA = 0.05', 'IOPT = 7', 'POTIM = 0', 'IBRION = 3',
+             'ISIF = 3','\nKPOINTS '+str(inital_kpoints)]
+    
+    step1 = ['\n1 Rough_Converge\n',
+             'EDIFFG = -0.05', 'ISYM = 0','NELMIN = 4','NELM = 60',
+             '\nKPOINTS '+str(final_kpoints)]
+    
+    step2 = ['\n2 Nonmag_Single\n',
+             'NSW = 0','ISPIN = 1','NELM = 250']
+    
+    step3 = ['\n3 Full_Converge\n',
+             'NSW = 500', 'ICHARG = 1', 'ISTART = 1', 'PREC = Accurate',
+             'EDIFFG = -1e-2', 'EDIFF = 1e-6','NELM = 100', 'ISPIN = 2']
+    
+    step4 = ['\n4 One_Step\n',
+             'LAECHG = .TRUE.','NSW = 0','NELM = 500','ICHARG = 0',
+             'LWAVE = False', 'LVHAR = True', 'ALGO = Normal','NELMIN = 10']
+    
+    return step0 + step1 + step2 + step3 + step4
+
+
+def old_bulk_convergence(inital_kpoints, final_kpoints, natoms):
     if natoms < 12:
         npar = 'NPAR = 3'
         kpar = 'KPAR = 4'
@@ -129,3 +227,4 @@ def band_structure_calculation():
              'NELM = 1000', 'NPAR = 1', 'NSW = 0','PREC = Accurate', 'SIGMA = 0.05']
     
     return step0
+
